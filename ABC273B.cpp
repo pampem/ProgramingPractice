13b7dff23673e08ff15e0571ef7d40998d45199e
Masashi <masashiizmt@gmail.com>
2022-11-19 03:08:12 +0900

    AtCoder Today >> 1118

--- ABC273B.cpp
+++ ABC273B.cpp
@@ -1,9 +1,22 @@
-//AtCoder today >> 1115
+//AtCoder today >> 1118
 //B - Broken Rounding
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-  
+  uint64_t X, K;
+
+  cin >> X >> K;
+
+  for(int i=0; i<K; i++){
+    X /= pow(10,i);
+    if(X % 10 <= 4){
+      X -= X%10;
+    }else{
+      X += 10-X%10;
+    }
+    X *= pow(10,i);
+  }
+  cout << X << endl;
 }
