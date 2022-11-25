7faaea3c164b4c1bdc37c9569c270cb23c0b624b
pampem <masashiizmt@gmail.com>
2022-11-25 20:01:49 +0900

    AtCoder Today >> 1125

--- ABC278D.cpp
+++ ABC278D.cpp
@@ -9,37 +9,81 @@ int main(){
   int N;
   cin >> N;
 
-  vector<int64_t> A(N);
+  vector<int> A(N);
 
-  for(int i=0; i<N; i++){
-    cin >> A.at(i);
-  }
+  // for(int i=0; i<N; i++){
+  //   cin >> A.at(i);
+  // }
+
+  for (int &a: A)
+       cin>>a;
+
+  map<int, int64_t> M;
+  int base = 0;
+  for(int i=0; i<N; i++)
+    M[i] = A.at(i);
 
   int Q;
   cin >> Q;
-  int indicater, xq, iq;
 
   for(int i=0; i<Q; i++){
-    cin >> indicater;
-
-    if(indicater == 1){
-      cin >> xq;
-      // vector<int64_t> X(N, xq);
-      // A = X;
-
-      rep(j,A.size()){
-        A.at(j) = xq;
-      }
-
-    }else if(indicater == 2){
-      cin >> iq >> xq;
-      A.at(iq-1) += xq;
-
-    }else{// == 3
-      cin >> iq;
-      cout << A.at(iq-1) << endl;
+    int t;
+    cin >> t;
+    if(t == 1)
+    {
+      int x;
+      cin >> x;
+      base = x;
+      M.clear();
+    }
+    else if(t == 2)
+    {
+      int i, x;
+      cin >> i >> x;
+      M[i-1] += x;
+    }
+    else
+    {
+      int i;
+      cin >> i;
+      cout << M[i-1] + base << endl;
     }
   }
+
+
+
+  //This code get AC in Sample, but in N == large condition, this get TLE.
+  // vector<int64_t> A(N);
+  //
+  // for(int i=0; i<N; i++){
+  //   cin >> A.at(i);
+  // }
+  //
+  // int Q;
+  // cin >> Q;
+  // int indicater, xq, iq;
+  //
+  // for(int i=0; i<Q; i++){
+  //   cin >> indicater;
+  //
+  //   if(indicater == 1){
+  //     cin >> xq;
+  //     // vector<int64_t> X(N, xq);
+  //     // A = X;
+  //
+  //     rep(j,A.size()){
+  //       A.at(j) = xq;
+  //     }
+  //
+  //   }else if(indicater == 2){
+  //     cin >> iq >> xq;
+  //     A.at(iq-1) += xq;
+  //
+  //   }else{// == 3
+  //     cin >> iq;
+  //     cout << A.at(iq-1) << endl;
+  //   }
+  // }
 
   return 0;
 }
