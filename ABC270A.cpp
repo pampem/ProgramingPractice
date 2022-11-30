d20a7bec22dd5e6b3ea755b7ecf131492fe7d1f3
pampem <masashiizmt@gmail.com>
2022-11-30 18:50:45 +0900

    AtCoder Today >> 1130

---
+++ ABC270A.cpp
@@ -0,0 +1,22 @@
+//AtCoder Today >> 1130
+//A - 1-2-4 Test
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main(){
+  int A, B;
+  cin >> A >> B;
+
+  int sunuke = A|B;
+
+  cout << sunuke << endl;
+
+  //点数の取りうる値は以下。
+  //0, 1, 2, 3, 4, 5, 6, 7
+
+
+
+  return 0;
+}
