ec89a327b1afaa35c02f448445ebfb260fb03862
Masashi <masashiizmt@gmail.com>
2022-12-03 17:53:17 +0900

    AtCoder Today >> 1203

---
+++ ABC269D.cpp
@@ -0,0 +1,20 @@
+//AtCoder Today >> 1203
+//D - Do use hexagon grid
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main(){
+  int N;
+  cin >> N;
+
+  vector<pair> black(N);
+  for(auto &sq:black){
+    cin >> sq.first >> sq.second;
+  }
+
+  
+
+  return 0;
+}
