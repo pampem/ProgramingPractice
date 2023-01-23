e6944f516dde5cefbbf98181216b88753fb3b3a1
pampem <masashiizmt@gmail.com>
2023-01-23 19:54:07 +0900

    230123

---
+++ ATC001A.cpp
@@ -0,0 +1,21 @@
+//AtCoder Today >> 230121
+//A - 深さ優先探索
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int search(char x,char y){
+    if()
+}
+int main(){
+    int h,w;
+    cin >> h >> w;
+
+    vector<string> s(h);
+
+    rep(i,h) cin >> s[i];
+
+    cout << s << endl;
+    return 0;
+}
