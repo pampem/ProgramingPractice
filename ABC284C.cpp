ef1ceb8152ff8e2d6fb4a2439d0c67e6f1739056
pam <masashiizmt@gmail.com>
2023-02-10 17:29:21 +0900

    230210

--- ABC284C.cpp
+++ ABC284C.cpp
@@ -5,7 +5,39 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
+int n, m;
+vector<int> visi;
+vector<vector<int>> G;
+
+void dfs(int c){
+  visi[c]=true;
+  for(auto &d:G[c]){
+    if(visi[d])continue;
+    dfs(d);
+  }
+}
+
 int main(){
-  
+  cin >> n >> m;
+  G.resize(n);
+  rep(i, m){
+    int u, v;
+    cin >> u >> v;
+    u--; v--;
+    G[u].push_back(v);
+    G[v].push_back(u);
+  }
+
+  int ans=0;
+  visi.resize(n);
+  rep(i, n){
+    if(!visi[i]){
+      ans++;
+      dfs(i);
+    }
+  }
+  cout << ans << endl;
+
+
   return 0;
 }
--- ABC288C.cpp
+++ ABC288C.cpp
@@ -6,41 +6,26 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-
-vector<bool> seen;
-vector<bool> finished;
-int kaburi;
-
-int dfs(const vector<vector<int>> &G, int v){
-  visited[v] = true;
-  for(auto next_v : G[v]){
-    if(visited[next_v]){
-      kaburi = next_v;
-      return 1;
-    }
-    dfs(G, next_v);
-  }
-  return 0;
-}
+//方針
+//連結成分に分ける
+//連結成分の数を数える
+//連結成分の数-1を出力する
 
 int main(){
-  int N, M;
+  int N,M;
   cin >> N >> M;
 
-  vector<vector<int>> G;
-  rep(i, M){
-    int a, b;
+  vector<vector<int>> G(N);
+  rep(i,M){
+    int a,b;
     cin >> a >> b;
+    a--;b--;
     G[a].push_back(b);
     G[b].push_back(a);
   }
 
-  visited.assign(N, false);
-  while(dfs(G, 0)){
-    //削除処理を書く。
-    G.erase(G.begin() + kaburi);
-  }
+  
 
-
+  
   return 0;
 }
