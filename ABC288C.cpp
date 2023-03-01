017f6b0ab6d3583d6a6712990ea27b82e5760c51
pam <masashiizmt@gmail.com>
2023-03-02 05:44:36 +0900

    230301

--- ABC288C.cpp
+++ ABC288C.cpp
@@ -6,16 +6,21 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-//方針
-//連結成分に分ける
-//連結成分の数を数える
-//連結成分の数-1を出力する
+void dfs(int v, vector<vector<int>> &G, vector<bool> &seen){
+  seen[v] = true;
+  for(auto v2:G[v]){
+    if(seen[v2])continue;
+    dfs(v2,G,seen);
+  }
+  return;
+}
 
 int main(){
   int N,M;
   cin >> N >> M;
-
   vector<vector<int>> G(N);
+  vector<bool> seen(N,false);
+  
   rep(i,M){
     int a,b;
     cin >> a >> b;
@@ -24,8 +29,15 @@ int main(){
     G[b].push_back(a);
   }
 
-  
+  int renketu = 0;
 
-  
+  rep(i,N){
+    if(seen[i])continue;
+    dfs(i,G,seen);
+    renketu++;
+  }
+
+  int ans = M-N+renketu;
+  cout << ans << endl;
   return 0;
 }
